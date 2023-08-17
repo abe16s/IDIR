315d3d6a64d56a846a8c8129b1030aafef4f4207
beka birhanu <beka18birhanu@gmail.com>
2023-08-17 18:48:45 +0300

    create ImageIcons

--- GUI/src/ImageIcons.java
+++ GUI/src/ImageIcons.java
@@ -5,11 +5,38 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public final class ImageIcons {
-    public static ImageIcon LOGO = new ImageIcon("Java-Version/src/GUI/images/game logo.png");
-    public static ImageIcon SETTING = new ImageIcon("Java-Version/src/GUI/images/setting.png");
-    public static ImageIcon GEAR = new ImageIcon("C:/Users/user/Downloads/icons8-settings-32 (1).png");
-    public static ImageIcon PLAY = new ImageIcon("C:/Users/user/Downloads/icons8-play-64.png");
-    public static ImageIcon MENU = new ImageIcon("C:/Users/user/Downloads/icons8-menu-32.png");
+    public static final ImageIcon OFFICIAL = new ImageIcon("GUI\\Icons\\colored\\administrator-male.png");
+    public static final ImageIcon OFFICIALdark = new ImageIcon("GUI\Icons\dark\administrator-male.png");
+
+    public static final ImageIcon COINS = new ImageIcon("GUI\\Icons\\colored\\coins.png");
+    public static final ImageIcon COINSdark = new ImageIcon("GUI\Icons\dark\coins.png");
+
+    public static final ImageIcon CONTACTS = new ImageIcon("GUI\Icons\colored\contacts.png");
+    public static final ImageIcon CONTACTSdark = new ImageIcon("GUI\Icons\dark\contacts.png");
+
+    public static final ImageIcon HELP = new ImageIcon("GUI\Icons\colored\help.png");
+    public static final ImageIcon HELPdark = new ImageIcon("GUI\Icons\dark\help.png");
+
+    public static final ImageIcon NOTE = new ImageIcon("GUI\Icons\colored\note.png");
+    public static final ImageIcon NOTEdark = new ImageIcon("GUI\Icons\dark\note.png");
+
+    public static final ImageIcon RULES = new ImageIcon("GUI\Icons\colored\rules.png");
+    public static final ImageIcon RULESdark = new ImageIcon("GUI\Icons\dark\rules.png");
+
+    public static final ImageIcon HOME = new ImageIcon("GUI\Icons\colored\home.png");
+    public static final ImageIcon HOMEdark = new ImageIcon("GUI\Icons\dark\home.png");
+
+    public static final ImageIcon LOGO = new ImageIcon("GUI\Icons\colored\family.png");
+    public static final ImageIcon LOGObackground = new ImageIcon("GUI\Icons\dark\family.png");
+
+    public static final ImageIcon EDIT = new ImageIcon("GUI\Icons\dark\edit-account.png");
+
+    public static final ImageIcon OFFICIAL = new ImageIcon("GUI\\Icons\\colored\\administrator-male.png");
+
+    public static final ImageIcon OFFICIAL = new ImageIcon("GUI\\Icons\\colored\\administrator-male.png");
+
+    public static final ImageIcon OFFICIAL = new ImageIcon("GUI\\Icons\\colored\\administrator-male.png");
+    
 
 
     public static ImageIcon reSizeImageIcon(ImageIcon icon, int width, int height){
