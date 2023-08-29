1a45f226318829e16abc43932e0e3477cb9a932b
Abenezer Seifu <abenezerseifu123@gmail.com>
2023-08-29 20:49:11 +0300

    Finalizing HomePage

--- GUI/src/App.java
+++ GUI/src/App.java
@@ -1,17 +1,7 @@
 package GUI.src;
 
-
-import java.awt.AWTEvent;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.MouseEvent;
-
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
 import javax.swing.UIManager;
-
-//import org.w3c.dom.events.MouseEvent;
-
 import GUI.src.utilities.*;
 
 public class App {
