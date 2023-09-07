14ab37feec23b366a59d0487708a8ce7bd42bfee
Abenezer Seifu <abenezerseifu123@gmail.com>
2023-09-07 08:23:47 +0300

    Adjustments to Database after working on the GUI
    Members
    Gender Attribute
    Create auto increment ID
    
    IDIR
    Total amount
    Type of bank along with the bank account no
    
    Properties
    Remove number of items in store
    
    Receipt
    Create auto increment Receipt No
    Change attribute name payer ID to Issued_For and Money Receiver to Issued_By
    Type – attribute – income/expenditure
    Deleted – attribute – If deleted or not
    
    Create Monthly Payment History Table
    That has a constant of 14 attribute Member Id, year and 12 months
    And all member IDs are added every year with the New Year and null for the months and automatically updated when the member pays for the specific month
    
    Agenda
    Create auto increment Agenda No

--- Database/Inintial_Table_Creation.sql
+++ Database/Inintial_Table_Creation.sql
@@ -2,13 +2,14 @@ CREATE SCHEMA IDIR;
 USE IDIR;
 
 CREATE TABLE MEMBER_TABLE (
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     First_Name VARCHAR(20) NOT NULL,
     Father_Name VARCHAR(20) NOT NULL,
     Grandfather_Name VARCHAR(20) NOT NULL,
     Member_Address VARCHAR(30) NOT NULL,
     Phone_No CHAR(10) UNIQUE,
     Age INT,
+    Gender VARCHAR(6),
     Occupation VARCHAR(20),
     Religion VARCHAR(15),
     Photo VARCHAR(255) UNIQUE
@@ -18,7 +19,8 @@ CREATE TABLE IDIR_TABLE (
     Official_Name VARCHAR(50) PRIMARY KEY,
     Office_Address VARCHAR(30) NOT NULL,
     Store_Address VARCHAR(30),
-    Bank_Acc_No VARCHAR(15) NOT NULL,
+    Bank_Acc_No VARCHAR(30) NOT NULL,
+    Total_Amount INT,
     Chairman INT,
     Vice_Chairman INT,
     Secretary INT,
@@ -35,7 +37,7 @@ CREATE TABLE IDIR_TABLE (
     Rules_And_Regulations VARCHAR(255),
     FOREIGN KEY (Chairman)
         REFERENCES MEMBER_TABLE (ID)
-		ON UPDATE CASCADE,
+        ON UPDATE CASCADE,
     FOREIGN KEY (Vice_Chairman)
         REFERENCES MEMBER_TABLE (ID)
         ON UPDATE CASCADE,
@@ -74,21 +76,22 @@ CREATE TABLE IDIR_TABLE (
 CREATE TABLE PROPERTY (
     Property_Type VARCHAR(15) PRIMARY KEY,
     Number_Of_Items INT,
-    In_Store INT,
     Individual_Price DECIMAL(6 , 2 )
 );
 
 CREATE TABLE RECEIPT (
-    Receipt_No INT PRIMARY KEY,
+    Receipt_No INT AUTO_INCREMENT PRIMARY KEY,
     Issued_Date DATE NOT NULL,
-    Payer_ID INT NOT NULL,
-    Money_Receiver INT NOT NULL,
+    Issued_For INT NOT NULL,
+    Issued_By INT NOT NULL,
     Reason_for_Payment VARCHAR(50) NOT NULL,
     Amount DECIMAL(4 , 2 ) NOT NULL,
-    FOREIGN KEY (Payer_ID)
+    Type VARCHAR(12),
+    Deleted VARCHAR(100) DEFAULT 'NO',
+    FOREIGN KEY (Issued_For)
         REFERENCES MEMBER_TABLE (ID)
         ON UPDATE CASCADE,
-    FOREIGN KEY (Money_Receiver)
+    FOREIGN KEY (Issued_By)
         REFERENCES MEMBER_TABLE (ID)
         ON UPDATE CASCADE
 );
@@ -107,7 +110,7 @@ CREATE TABLE FAMILY (
 );
   
 CREATE TABLE AGENDA (
-    Agenda_No INT PRIMARY KEY,
+    Agenda_No INT AUTO_INCREMENT PRIMARY KEY,
     Written_Date DATE,
     Title VARCHAR(20),
     Writer INT NOT NULL,
@@ -117,29 +120,55 @@ CREATE TABLE AGENDA (
         ON UPDATE CASCADE
 );
 
-CREATE VIEW PAYMENT_HISTORY AS
-    SELECT 
-        M.ID, R.Receipt_No, R.Reason_for_Payment
-    FROM
-        MEMBER_TABLE AS M,
-        RECEIPT AS R
-    WHERE
-        M.ID = R.Payer_ID;
+CREATE TABLE MONTHLY_PAYMENT_HISTORY (
+    ID INT,
+    Year CHAR(4),
+    Jan INT,
+    Feb INT,
+    Mar INT,
+    Apr INT,
+    May INT,
+    Jun INT,
+    Jul INT,
+    Aug INT,
+    Sep INT,
+    Oct INT,
+    Nov INT,
+    `Dec` INT,
+    FOREIGN KEY (ID)
+        REFERENCES MEMBER_TABLE (ID),
+    FOREIGN KEY (Jan)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Feb)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Mar)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Apr)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (May)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Jun)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Jul)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Aug)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Sep)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Oct)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (Nov)
+        REFERENCES RECEIPT (Receipt_No),
+    FOREIGN KEY (`Dec`)
+        REFERENCES RECEIPT (Receipt_No),
+    PRIMARY KEY (ID , Year)
+);
         
 DELIMITER $$
 
 CREATE TRIGGER MEMBER_DELETE BEFORE DELETE ON MEMBER_TABLE FOR EACH ROW
 BEGIN
-  SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You can\''t delete members'; 
-END $$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER RECEIPT_DELETE BEFORE DELETE ON RECEIPT FOR EACH ROW
-BEGIN
-  SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You can\''t delete receipts'; 
+  SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "You can't delete members"; 
 END $$
 
 DELIMITER ;
