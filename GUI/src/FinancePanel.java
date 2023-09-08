68d236a98823cad1f921b51118bea6252cecf894
Abenezer Seifu <abenezerseifu123@gmail.com>
2023-09-08 17:46:41 +0300

    Initial Connection of finance page to local database

--- GUI/src/FinancePanel.java
+++ GUI/src/FinancePanel.java
@@ -6,9 +6,17 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Insets;
 import java.io.File;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -40,18 +48,73 @@ public class FinancePanel extends JPanel implements ParentPanel {
         double curYearIncome = 100000;
         double curYearExpenditure = 30000;
 
-        String[][] properties = {
-            {"Chair", "10", "5500"},
-            {"Table", "5", "7700"},
-            {"Tent", "3", "15000"},
-            {"Casserole", "15", "1000"},
-            {"Chair", "10", "5500"},
-            {"Table", "5", "7700"},
-            {"Tent", "3", "15000"},
-            {"Casserole", "15", "1000"}
-        };
+        Object[] basicInfo = {officialName, accountNo, totalAmount};
+        Double[] curMonthData = {curMonthIncome, curMonthExpenditure};
+        Double[] curYearData = {curYearIncome, curYearExpenditure};
+ 
+        try (Statement generalsStmt = App.DATABASE_CONNECTION.createStatement()) {
+            ResultSet retrieveIdirInfo = generalsStmt.executeQuery("SELECT Official_Name, Bank_Acc_No, Total_Amount From IDIR_TABLE;");
+            while (retrieveIdirInfo.next()) {
+                basicInfo[0] = retrieveIdirInfo.getString(1);
+                basicInfo[1] = retrieveIdirInfo.getString(2);
+                basicInfo[2] = retrieveIdirInfo.getDouble(3);
+            }
 
-        String[][] expendings = {
+            ResultSet retrieveCurMonthData = generalsStmt.executeQuery("SELECT Type, Sum(Amount) " + //
+                    "FROM RECEIPT " + //
+                    "WHERE MONTH(Issued_Date) = MONTH(curdate()) AND YEAR(Issued_Date) = YEAR(curdate()) AND Deleted = 'NO' " + //
+                    "GROUP BY Type " + //
+                    "ORDER BY Type DESC;");
+
+            int type = 0;
+            while (retrieveCurMonthData.next()) {
+                curMonthData[type] = retrieveCurMonthData.getDouble(2);
+                type++;
+            } 
+
+            ResultSet retrieveCurYearData = generalsStmt.executeQuery("SELECT Type, Sum(Amount) " + //
+                    "FROM RECEIPT " + //
+                    "WHERE YEAR(Issued_Date) = YEAR(curdate()) AND Deleted = 'NO' " + //
+                    "GROUP BY Type " + //
+                    "ORDER BY Type DESC;");
+
+            type = 0;
+            while (retrieveCurYearData.next()) {
+                curYearData[type] = retrieveCurYearData.getDouble(2);
+                type++;
+            } 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Object[][] properties = {
+            {"Chair", 10, 5500},
+            {"Table", 5, 7700},
+            {"Tent", 3, 15000},
+            {"Casserole", 15, 1000},
+            {"Chair", 10, 5500},
+            {"Table", 5, 7700},
+            {"Tent", 3, 15000},
+            {"Casserole", 15, 1000}
+        };
+        int ctr = 0;
+        try (Statement propertiesStmt = App.DATABASE_CONNECTION.createStatement()) {
+            ResultSet retrieveProperties = propertiesStmt.executeQuery("SELECT Property_Type, Number_Of_Items, Individual_Price " + //
+                    "FROM PROPERTY; ");
+            ArrayList<Object[]> curProperties = new ArrayList<Object[]>();
+            ctr = 0;
+            while (retrieveProperties.next()) {
+                ctr++;
+                Object[] cur = {retrieveProperties.getString(1), retrieveProperties.getInt(2), retrieveProperties.getDouble(3)};
+                curProperties.add(cur);
+            }
+            if (ctr > 0)
+                properties = curProperties.toArray(Object[][]::new);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Object[][] expendings = {
             {"03/05/2021", "000039",  "3000", "Death of Brother"},
             {"23/05/2021", "000111", "5500", "Buying of chair"},
             {"03/05/2021", "000139", "3000", "Electrict utility"},
@@ -79,29 +142,86 @@ public class FinancePanel extends JPanel implements ParentPanel {
             {"23/05/2021", "000111", "5500", "Buying of chair"},
             {"03/05/2021", "000139", "3000", "Electrict utility"},
             {"03/05/2021", "003539", "3000", "Death of Sister"}
-        }; //Should be sorted by date when retrieved
-
-        String[][] yearlyIncomeExpendings = {
-            {"2023 (Income)","10000","12000","10020","10000","12000","10020","10000","12000","10020","10000","12000","10020", "150,000"},
-            {"2023 (Expendings)", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "30,000"},
-            {"2022 (Income)","10000","12000","10020","10000","12000","10020","10000","12000","10020","10000","12000","10020", "150,000"},
-            {"2022 (Expendings)", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "30,000"},
-            {"2021 (Income)","10000","12000","10020","10000","12000","10020","10000","12000","10020","10000","12000","10020", "150,000"},
-            {"2021 (Expendings)", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "30,000"},
-            {"2020 (Income)","10000","12000","10020","10000","12000","10020","10000","12000","10020","10000","12000","10020", "150,000"},
-            {"2020 (Expendings)", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "5000","2000","3000", "30,000"},
         };
-        
+
+        try (Statement expendingsStmt = App.DATABASE_CONNECTION.createStatement()) {
+            ResultSet retrieveExpendings = expendingsStmt.executeQuery("SELECT Issued_Date, LPAD(Receipt_No, 6, '0'), Amount, Reason_for_Payment " + //
+                    "FROM Receipt " + //
+                    "WHERE Type = 'Expenditure' AND Deleted = 'NO' " + //
+                    "ORDER BY Issued_Date DESC;");
+            ArrayList<Object[]> curExpendings = new ArrayList<Object[]>();
+            ctr = 0;
+            while (retrieveExpendings.next()) {
+                ctr++;
+                Object[] cur = {retrieveExpendings.getDate(1), retrieveExpendings.getString(2), retrieveExpendings.getDouble(3), retrieveExpendings.getString(4)};
+                curExpendings.add(cur);
+            }
+            if (ctr > 0) 
+                expendings = curExpendings.toArray(Object[][]::new);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }        
 
         LocalDate currentDate = LocalDate.now();
+        int yearInterval = 1;
 
-        queryPanel officialNamePanel = new queryPanel("Official Name", officialName, getBackground());
-        queryPanel accountNoPanel = new queryPanel("Bank Account No", accountNo, getBackground());
-        queryPanel totalAmountPanel = new queryPanel("Total Amount", Double.toString(totalAmount) + " Birr", getBackground());
-        queryPanel curMonthIncomePanel = new queryPanel(currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "'s total income till date", Double.toString(curMonthIncome) + " Birr", getBackground());
-        queryPanel curMonthExpenditurePanle = new queryPanel(currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "'s total expenditure till date", Double.toString(curMonthExpenditure) + " Birr", getBackground());
-        queryPanel curYearIncomePanel = new queryPanel(Integer.toString(currentDate.getYear()) + "'s total income till date", Double.toString(curYearIncome) + " Birr", getBackground());
-        queryPanel curYearExpenditurePanel = new queryPanel(Integer.toString(currentDate.getYear()) + "'s total expenditure till date", Double.toString(curYearExpenditure) + " Birr", getBackground());
+        try {
+            Statement startingDateStmt = App.DATABASE_CONNECTION.createStatement();
+            ResultSet startingDate = startingDateStmt.executeQuery("SELECT YEAR(Starting_Date) FROM IDIR_TABLE;");
+            while (startingDate.next()) {
+                yearInterval = currentDate.getYear() - startingDate.getInt(1) + 1 ;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Object[][] yearlyIncomeExpendings = new Object[yearInterval*2][];
+        int j = 0;
+        for (int i = currentDate.getYear(); i >= currentDate.getYear()-yearInterval+1; ) {
+            Object[] cur = new Object[14];
+            if (j % 2 == 0) {
+                cur[0] = Integer.toString(i) + " (Income)";
+            } else {
+                cur[0] = Integer.toString(i) + " (Expendings)";
+                i--;
+            }
+            for (int k = 1; k < 14; k++) {
+                cur[k] = 0;
+            }
+            yearlyIncomeExpendings[j++] = cur;
+        }
+        
+
+        try (Statement reportStmt = App.DATABASE_CONNECTION.createStatement()) {
+            ResultSet retrieveReport = reportStmt.executeQuery("Select YEAR(Issued_Date), MONTH(Issued_Date), Type, SUM(Amount) " + //
+                    "From RECEIPT " + //
+                    "Where Deleted = 'NO' " + //
+                    "GROUP BY MONTH(Issued_Date), YEAR(Issued_Date), Type");
+            
+            while (retrieveReport.next()) {
+                int yr = currentDate.getYear() - retrieveReport.getInt(1);
+                yr *= 2;
+                if (retrieveReport.getString(3).equalsIgnoreCase("Expenditure")) {
+                    yr++;
+                }
+                int mnt = retrieveReport.getInt(2);
+                if (yr < 0 || yr >= yearInterval*2) continue;
+                Double amt = retrieveReport.getDouble(4);
+                yearlyIncomeExpendings[yr][mnt] = amt;
+                yearlyIncomeExpendings[yr][13] = Double.valueOf(yearlyIncomeExpendings[yr][13].toString()) + amt;
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        queryPanel officialNamePanel = new queryPanel("Official Name", basicInfo[0].toString(), getBackground());
+        queryPanel accountNoPanel = new queryPanel("Bank Account No", basicInfo[1].toString(), getBackground());
+        queryPanel totalAmountPanel = new queryPanel("Total Amount", Double.toString((Double)basicInfo[2]) + " Birr", getBackground());
+        queryPanel curMonthIncomePanel = new queryPanel(currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "'s total income till date", Double.toString(curMonthData[0]) + " Birr", getBackground());
+        queryPanel curMonthExpenditurePanle = new queryPanel(currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "'s total expenditure till date", Double.toString(curMonthData[1]) + " Birr", getBackground());
+        queryPanel curYearIncomePanel = new queryPanel(Integer.toString(currentDate.getYear()) + "'s total income till date", Double.toString(curYearData[0]) + " Birr", getBackground());
+        queryPanel curYearExpenditurePanel = new queryPanel(Integer.toString(currentDate.getYear()) + "'s total expenditure till date", Double.toString(curYearData[1]) + " Birr", getBackground());
         
         generalData = new JPanel();
         generalData.setLayout(new BoxLayout(generalData, BoxLayout.Y_AXIS));
@@ -143,7 +263,7 @@ public class FinancePanel extends JPanel implements ParentPanel {
         
         JPanel yearlyReportPanel = new JPanel(new BorderLayout());
         yearlyReportPanel.setBackground(getBackground());
-        String[] columnNames3 = {"Year", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Total"};
+        String[] columnNames3 = {"Year", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Total"};
         CustomTable yearlyIncomeExpendingsTable = new CustomTable(this, yearlyIncomeExpendings, columnNames3);
         yearlyIncomeExpendingsTable.setAlternatingColor(Color.LIGHT_GRAY, new Color(228, 228, 228), Color.WHITE);
         JScrollPane scrollYearly = new JScrollPane(yearlyIncomeExpendingsTable);
--- GUI/src/RulesPanel.java
+++ GUI/src/RulesPanel.java
@@ -3,6 +3,9 @@ package GUI.src;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Insets;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,6 +14,7 @@ import GUI.src.SkeletalWindow.BasePanel;
 import GUI.src.utilities.ReadAndDisplay;
 
 public class RulesPanel extends JPanel{
+    private String rules = "GUI\\src\\utilities\\Sample Rule.txt";
 
     public RulesPanel(BasePanel displayPanel) {
         setLayout(new BorderLayout());
@@ -18,8 +22,20 @@ public class RulesPanel extends JPanel{
         setBorder(new EmptyBorder(new Insets(5, 10, 0, 0)));
 
         ReadAndDisplay readAndDisplay = new ReadAndDisplay();
-        readAndDisplay.readFile("GUI\\src\\utilities\\Sample Rule.txt");
+        readAndDisplay.readFile(getRules());
         readAndDisplay.setBackground(getBackground());
         add(readAndDisplay.getWhole(), BorderLayout.CENTER);
+    }
+
+    private String getRules() {
+        try (Statement statement = App.DATABASE_CONNECTION.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT Rules_And_Regulations FROM IDIR_TABLE");
+            while (resultSet.next()) {
+                rules = resultSet.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rules;
     }
 }
